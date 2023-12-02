eb2613ea65347cb3763b1f872dd8131ab1c7d918
VictorVictini <84572057+VictorVictini@users.noreply.github.com>
2023-12-02 17:30:40 +0000

    day 2 solutions

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@
 namespace AdventOfCode2023 {
     public class Program {
         private static void Main() {
-            int dayNum = WithinLimit(1);
+            int dayNum = WithinLimit(2);
             switch (dayNum) {
                 case 1:
                     new Day1().OutputParts();
+                    break;
+                case 2:
+                    new Day2().OutputParts();
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(dayNum), "Encountered a day not in the switch statement");
