d2c5e24c8eeb9ef3d8c18b99cbc6b3c146379289
VictorVictini <84572057+VictorVictini@users.noreply.github.com>
2023-12-06 09:37:13 +0000

    outputting time taken alongside solutions

--- days/Day.cs
+++ days/Day.cs
@@ -1,8 +1,25 @@
+using System.Diagnostics;
+
 namespace AdventOfCode2023 {
     public abstract class Day<T> {
         public void OutputParts() {
-            Console.WriteLine("Part 1 is " + this.Part1());
-            Console.WriteLine("Part 2 is " + this.Part2());
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            T res1 = this.Part1();
+            watch.Stop();
+            TimeSpan ts = watch.Elapsed;
+            string time1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            watch.Start();
+            T res2 = this.Part2();
+            watch.Stop();
+            ts = watch.Elapsed;
+            string time2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+
+            Console.WriteLine("Part 1 took {0}ms and returned: {1}", time1, res1);
+            Console.WriteLine("Part 2 took {0}ms and returned: {1}", time2, res2);
         }
         public abstract T Part1();
         public abstract T Part2();
